1ce1e804a076f91888252a7e09c5e951ee416621
Asal <asal.mehradfar@yahoo.com>
2024-11-23 20:59:15 -0800

    receiver mixing sim results

--- simulation.py
+++ simulation.py
@@ -13,21 +13,26 @@ circuit_params = get_circ_params(args.circuit)
 circuit_path, circuit_path_docker = get_circ_path(args.circuit)
 params_path = get_dataset_path(args.circuit, args.model)
 
-simulator = Simulator(circuit_path, circuit_path_docker, circuit_params, params_path)
-
+simulator = Simulator(circuit_path, circuit_path_docker, circuit_params, params_path, 'oceanScript.ocn')
 simulator.run_all(n=args.npoints)
+results = simulator.sim_results
+
+if args.circuit == 'Receiver':
+   simulator_individual = Simulator(circuit_path, circuit_path_docker, circuit_params, params_path, 'oceanScriptIndividual.ocn')
+   simulator_individual.run_all(n=args.npoints)   
+   for i in range(len(results)):
+      results[i].update(simulator_individual.sim_results[i])
+
 
 # print simulation results
-
 # print(simulator.sim_results)
-
 # result.calc_hist(simulator.sim_results)
 # for item in simulator.sim_results:
 #    print(item)
 
 p = make_save_path(f'Dataset/{args.circuit}', args.model)
 with open(join(p, 'sim_results.csv'),'w', newline='') as f:
-   w = csv.DictWriter(f, simulator.sim_results[-1].keys())
+   w = csv.DictWriter(f, results[-1].keys())
    w.writeheader()
-   w.writerows(simulator.sim_results)
+   w.writerows(results)
    f.close()
